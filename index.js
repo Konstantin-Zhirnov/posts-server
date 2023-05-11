def2f1497dff6d07d79d319dffa33425c762b3d4
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-05-10 19:40:19 -0700

    Change url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://kostya-zhirnov.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://react-application-ci-cd.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   res.header('Access-Control-Allow-Credentials', true);
